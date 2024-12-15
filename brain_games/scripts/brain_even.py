1e476ea55765252cff2694a8abd01a840e1e8e7d
NikLuki <niklukic@hotmail.com>
2024-12-15 15:55:45 +0300

    fix brain_even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,25 +1,29 @@
 import random
 
 def main():
-    print('Welcome to the Brain Even Game!')
+    print('Welcome to the Brain Games!')
     name = input('May I have your name? ')
     print(f'Hello, {name}!')
-    
-    rounds = 3
-    for _ in range(rounds):
-        number = random.randint(1, 100)
-        correct_answer = 'yes' if number % 2 == 0 else 'no'
-        print(f'Question: {number}')
-        answer = input("Your answer (yes/no): ")
-        
-        if answer == correct_answer:
-            print('Correct!')
-        else:
-            print(f'Wrong! The correct answer was {correct_answer}.')
-            break
-    else:
-        print('Congratulations, you won!')
+    print(f'Answer "yes" if the number is even, otherwise answer "no".')
 
+    correct_answers = 0
+    while correct_answers < 3:
+        number = random.randint(1, 100)  # Генерация случайного числа
+        print(f"Question: {number}")
+        answer = input('Your answer: ')
+
+        correct_answer = 'yes' if number % 2 == 0 else 'no'
+        
+        if answer.lower() == correct_answer:
+            print("Correct!")
+            correct_answers += 1
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            return  # Завершение игры при неправильном ответе
+
+    print(f"Congratulations, {name}!")  # Поздравление при успешной игре
+    
 if __name__ == '__main__':
     main()
 
