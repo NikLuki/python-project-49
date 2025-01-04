e9d052a24ea4ea0f848d53d74a9a0921969ed6f3
NikLuki <niklukic@hotmail.com>
2025-01-04 20:15:36 +0300

    fix brain_prime.py description

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -1,6 +1,6 @@
 import random
 
-DESCRIPTION = '/Answer "yes" if the given number is prime. Otherwise answer "no"./'
+DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def is_prime(n):
     if n < 2:
